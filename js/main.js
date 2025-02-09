2ad3d6999df592b9cf647639c8fe76c75860013d
Ahmed Ibrahim <ahmedibrahim2621998@gmail.com>
2025-02-09 16:55:57 +0200

    edit in main js

--- js/main.js
+++ js/main.js
@@ -386,7 +386,7 @@ function translate(language) {
     let allDom = document.querySelectorAll("*");
 
     fetch(
-        `https://raw.githubusercontent.com/ahmed-ibrahim1998/IBN-Eljazeri-Website/languages/${lang}.json`
+        `https://raw.githubusercontent.com/ahmed-ibrahim1998/master/IBN-Eljazeri-Website/languages/${lang}.json`
     )
         .then((response) => {
             return response.json();
